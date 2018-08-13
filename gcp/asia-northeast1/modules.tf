db26b8870b1493422443975304dbda2ec5ba2ca9
ikeikeikeike <jp.ne.co.jp@gmail.com>
2018-08-14 00:12:35 +0900

    cloud sql

--- gcp/asia-northeast1/modules.tf
+++ gcp/asia-northeast1/modules.tf
@@ -47,3 +47,22 @@
 module "gcs" {
   source = "../../modules/gcp/gcs"
 }
+
+module "lit-db" {
+  source           = "github.com/GoogleCloudPlatform/terraform-google-sql-db"
+  tier             = "idb-f1-micro"
+  name             = "lit"
+  db_name          = "lit"
+  region           = "${lookup(var.region, "${terraform.env}")}"
+  user_name        = "${data.vault_generic_secret.lit-db.data["username"]}"
+  user_password    = "${data.vault_generic_secret.lit-db.data["password"]}"
+  db_collation     = "ja_JP.UTF8"
+  database_version = "POSTGRES_9_6"
+
+  # ip_configuration = [{
+    # authorized_networks = [{
+      # name  = "default"
+      # value = "${data.google_compute_subnetwork.default-us-central1.ip_cidr_range}"
+    # }]
+  # }]
+}
