9f2579074363a5b89e68c2068e383046e5241bce
ikeikeikeike <jp.ne.co.jp@gmail.com>
2018-10-05 01:25:42 +0900

    ssl certification

--- gcp/asia-northeast1/outputs.tf
+++ gcp/asia-northeast1/outputs.tf
@@ -1,3 +1,12 @@
 
 output "dns_lit_private_key_pem"  { value = "${module.dns.lit_private_key_pem}" }
+resource "local_file" "dns_lit_private_key_pem" {
+    content  = "${module.dns.lit_private_key_pem}"
+    filename = "${path.module}/lit.key"
+}
+
 output "dns_lit_certificate_pem"  { value = "${module.dns.lit_certificate_pem}" }
+resource "local_file" "dns_lit_certificate_pem" {
+    content  = "${module.dns.lit_certificate_pem}"
+    filename = "${path.module}/lit.cert"
+}
