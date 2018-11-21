6915d061a3290b11efe60a0a16b26930197a0282
ikeikeikeike <jp.ne.co.jp@gmail.com>
2018-11-22 02:20:24 +0900

    write certificate chain to local

--- gcp/asia-northeast1/outputs.tf
+++ gcp/asia-northeast1/outputs.tf
@@ -10,3 +10,9 @@ resource "local_file" "dns_lit_certificate_pem" {
     content  = "${module.dns.lit_certificate_pem}"
     filename = "${path.module}/lit.cert"
 }
+
+output "dns_lit_issuer_pem"  { value = "${module.dns.lit_issuer_pem}" }
+resource "local_file" "dns_lit_issuer_pem" {
+    content  = "${module.dns.lit_certificate_pem}${module.dns.lit_issuer_pem}"
+    filename = "${path.module}/lit.chain"
+}
--- modules/gcp/dns/outputs.tf
+++ modules/gcp/dns/outputs.tf
@@ -1,3 +1,4 @@
 output "lit_private_key_pem"  { value = "${acme_certificate.lit.private_key_pem}" }
 output "lit_certificate_pem"  { value = "${acme_certificate.lit.certificate_pem}" }
+output "lit_issuer_pem"       { value = "${acme_certificate.lit.issuer_pem}" }
 
