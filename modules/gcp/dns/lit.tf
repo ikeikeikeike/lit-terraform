d36f5a358555a291878063bd38ef0dcd2c67ac03
ikeikeikeike <jp.ne.co.jp@gmail.com>
2019-01-16 02:55:29 +0900

    google mx record

--- modules/gcp/dns/lit.tf
+++ modules/gcp/dns/lit.tf
@@ -11,3 +11,18 @@ resource "google_dns_record_set" "lit" {
   ttl          = 300
   rrdatas      = ["${var.vpc_global_address_lit_lb_address}"]
 }
+
+resource "google_dns_record_set" "lit_mx" {
+  managed_zone = "${google_dns_managed_zone.lit.name}"
+  name         = "${google_dns_managed_zone.lit.dns_name}"
+
+  type         = "MX"
+  ttl          = 3600
+  rrdatas = [
+    "1 aspmx.l.google.com.",
+    "5 alt1.aspmx.l.google.com.",
+    "5 alt2.aspmx.l.google.com.",
+    "10 alt3.aspmx.l.google.com.",
+    "10 alt4.aspmx.l.google.com."
+  ]
+}
